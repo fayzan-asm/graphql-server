8ffc0a54c7880c239de635b66d0921a074ec4adf
Muhammad Fayzan Siddiqui <muhammadfayzan.siddiqui@bertelsmann.de>
2024-10-07 13:16:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+});
+
+console.log(`Server ready at: ${url}`);
